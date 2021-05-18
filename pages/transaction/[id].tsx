7e7d25f8b225fa1f4855f701a16c7daf476f2a41
Jon Diep <jondiep1@gmail.com>
2021-05-18 01:41:02 -0700

    change var name

--- pages/transaction/[id].tsx
+++ pages/transaction/[id].tsx
@@ -84,7 +84,7 @@ export default function Transaction({ tx, network }: TransactionProps) {
 
 export async function getServerSideProps({ params, query }) {
   const { data: tx } = await axios.get(
-    `${process.env.HOST || process.env.VERCEL_URL}/api/transaction/${params.id}?network=${query.network}`
+    `${process.env.HOST || process.env.NEXT_PUBLIC_VERCEL_URL}/api/transaction/${params.id}?network=${query.network}`
   )
   return {
     props: { tx, network: query.network },
