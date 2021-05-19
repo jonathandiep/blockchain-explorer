d33de81e082bb483978146daf1977b341cc18a7f
Jon Diep <jondiep1@gmail.com>
2021-05-18 23:12:17 -0700

    .env updates

--- util/index.ts
+++ util/index.ts
@@ -4,5 +4,5 @@ export function getNetworkUrl(network: string, protocol: 'http' | 'ws' = 'http')
   }
 
   // FIXME: Use dotenv and import id
-  return `${protocol}s://${network}.infura.io/v3/6012542b9f064a18936d3447dd20b483`
+  return `${protocol}s://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
 }
