2013e422377eaadcda5baec58e36f122abc363c1
Jon Diep <jondiep1@gmail.com>
2021-06-02 22:26:22 -0700

    feat: dark mode

--- components/NetworkSelection.tsx
+++ components/NetworkSelection.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Box, Button } from '@chakra-ui/react'
+import { Box, Button, Flex, Spacer, useColorMode } from '@chakra-ui/react'
 
 interface NetworkSelectionProps {
   network: string
@@ -7,55 +7,68 @@ interface NetworkSelectionProps {
 }
 
 export default function NetworkSelection({ network }: NetworkSelectionProps) {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <Box marginTop={2}>
-      <Link href="/?network=mainnet">
-        <a>
-          <Button colorScheme="teal" size="xs" marginRight={2} variant={network === 'mainnet' ? 'solid' : 'outline'}>
-            Mainnet
-          </Button>
-        </a>
-      </Link>
-      <Link href="/?network=ropsten">
-        <a>
-          <Button colorScheme="red" size="xs" marginRight={2} variant={network === 'ropsten' ? 'solid' : 'outline'}>
-            Ropsten
-          </Button>
-        </a>
-      </Link>
-      <Link href="/?network=kovan">
-        <a>
-          <Button colorScheme="purple" size="xs" marginRight={2} variant={network === 'kovan' ? 'solid' : 'outline'}>
-            Kovan
-          </Button>
-        </a>
-      </Link>
-      <Link href="/?network=rinkeby">
-        <a>
-          <Button colorScheme="yellow" size="xs" marginRight={2} variant={network === 'rinkeby' ? 'solid' : 'outline'}>
-            Rinkeby
-          </Button>
-        </a>
-      </Link>
-      <Link href="/?network=goerli">
-        <a>
-          <Button
-            colorScheme="messenger"
-            size="xs"
-            marginRight={2}
-            variant={network === 'goerli' ? 'solid' : 'outline'}
-          >
-            Goerli
-          </Button>
-        </a>
-      </Link>
-      <Link href="/?network=localhost">
-        <a>
-          <Button colorScheme="blackAlpha" size="xs" variant={network === 'localhost' ? 'solid' : 'outline'}>
-            Localhost 8545
-          </Button>
-        </a>
-      </Link>
-    </Box>
+    <Flex>
+      <Box marginTop={2}>
+        <Link href="/?network=mainnet">
+          <a>
+            <Button colorScheme="teal" size="xs" marginRight={2} variant={network === 'mainnet' ? 'solid' : 'outline'}>
+              Mainnet
+            </Button>
+          </a>
+        </Link>
+        <Link href="/?network=ropsten">
+          <a>
+            <Button colorScheme="red" size="xs" marginRight={2} variant={network === 'ropsten' ? 'solid' : 'outline'}>
+              Ropsten
+            </Button>
+          </a>
+        </Link>
+        <Link href="/?network=kovan">
+          <a>
+            <Button colorScheme="purple" size="xs" marginRight={2} variant={network === 'kovan' ? 'solid' : 'outline'}>
+              Kovan
+            </Button>
+          </a>
+        </Link>
+        <Link href="/?network=rinkeby">
+          <a>
+            <Button
+              colorScheme="yellow"
+              size="xs"
+              marginRight={2}
+              variant={network === 'rinkeby' ? 'solid' : 'outline'}
+            >
+              Rinkeby
+            </Button>
+          </a>
+        </Link>
+        <Link href="/?network=goerli">
+          <a>
+            <Button
+              colorScheme="messenger"
+              size="xs"
+              marginRight={2}
+              variant={network === 'goerli' ? 'solid' : 'outline'}
+            >
+              Goerli
+            </Button>
+          </a>
+        </Link>
+        <Link href="/?network=localhost">
+          <a>
+            <Button colorScheme="telegram" size="xs" variant={network === 'localhost' ? 'solid' : 'outline'}>
+              Localhost 8545
+            </Button>
+          </a>
+        </Link>
+      </Box>
+      <Spacer />
+      <Button mt="2" size="xs" onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
+    </Flex>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from '../lib/theme'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
